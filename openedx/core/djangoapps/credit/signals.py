3f9716e87411a2a6f9a67f661bf118e2c4edd9b8
aamir-khan <aamir@edx.org>
2015-08-03 20:19:56 +0500

    ECOM-1980: updated the contents

--- openedx/core/djangoapps/credit/signals.py
+++ openedx/core/djangoapps/credit/signals.py
@@ -9,11 +9,16 @@ from django.utils import timezone
 from opaque_keys.edx.keys import CourseKey
 
 from xmodule.modulestore.django import SignalHandler
+from openedx.core.djangoapps.credit.partition_schemes import VerificationPartitionScheme
 from openedx.core.djangoapps.signals.signals import GRADES_UPDATED
-
+from xmodule.modulestore.django import modulestore
+from xmodule.modulestore import ModuleStoreEnum
+from xmodule.modulestore.exceptions import ItemNotFoundError
 
 log = logging.getLogger(__name__)
 
+# XBlocks that can be added as credit requirements
+CREDIT_REQUIREMENT_XBLOCK_CATEGORIES = "edx-reverification-block"
 
 def on_course_publish(course_key):  # pylint: disable=unused-argument
     """
@@ -27,6 +32,7 @@ def on_course_publish(course_key):  # pylint: disable=unused-argument
     # Import here, because signal is registered at startup, but items in tasks
     # are not yet able to be loaded
     from openedx.core.djangoapps.credit import api, tasks
+    tag_contents_with_partitions(course_key)
 
     if api.is_credit_course(course_key):
         tasks.update_credit_course_requirements.delay(unicode(course_key))
@@ -70,3 +76,52 @@ def listen_for_grade_calculation(sender, username, grade_summary, course_key, de
                     api.set_credit_requirement_status(
                         username, course_id, 'grade', 'grade', status="failed", reason={}
                     )
+
+
+def tag_contents_with_partitions(course_key):
+    # just an example code
+    # persist the value as a course tag
+    # course_tag_api.set_course_tag(user, course_key, partition_key, group.id)
+
+    credit_reqs_xblocks = _get_credit_requirements_xblocks(course_key)
+    for xblock in credit_reqs_xblocks:
+        partition = VerificationPartitionScheme()
+        xblock.group_access = {
+            partition.key_for_partition(xblock.get_credit_requirement_name()): [
+                partition.VERIFIED_ALLOW, partition.VERIFIED_DENY
+            ]
+        }
+        xblock.save()
+
+    pass
+
+
+def _get_credit_requirements_xblocks(course_key):
+    """
+    Retrieve all XBlocks in the course for a particular category.
+
+    Returns only XBlocks that are published and haven't been deleted.
+    """
+    xblocks = [
+        block for block in modulestore().get_items(
+            course_key,
+            qualifiers={"category": CREDIT_REQUIREMENT_XBLOCK_CATEGORIES},
+            revision=ModuleStoreEnum.RevisionOption.published_only,
+        )
+        if _is_in_course_tree(block)
+    ]
+    return xblocks
+
+
+def _is_in_course_tree(block):
+    """
+    Check that the XBlock is in the course tree.
+
+    It's possible that the XBlock is not in the course tree
+    if its parent has been deleted and is now an orphan.
+    """
+    ancestor = block.get_parent()
+    while ancestor is not None and ancestor.location.category != "course":
+        ancestor = ancestor.get_parent()
+
+    return ancestor is not None
