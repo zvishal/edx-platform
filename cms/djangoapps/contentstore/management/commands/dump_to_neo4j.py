82ba477ba300573735f1010f7cc0dc73bab66dff
Adam Palay <adam@edx.org>
2015-12-14 14:23:18 -0500

    write to tsv

--- cms/djangoapps/contentstore/management/commands/dump_to_neo4j.py
+++ cms/djangoapps/contentstore/management/commands/dump_to_neo4j.py
@@ -62,12 +62,6 @@ class Command(BaseCommand):
 
                 fields['display_name'] = item.display_name_with_default
 
-                if hasattr(item, 'course_id'):
-                    fields['org'] = item.course_id.org
-                    fields['course'] = item.course_id.course
-                    fields['run'] = item.course_id.run
-                    fields['course_id'] = unicode(item.course_id)
-
                 fields['location:ID'] = unicode(item.location)
                 if "location" in fields:
                     del fields['location']
@@ -79,9 +73,15 @@ class Command(BaseCommand):
                 fields['type:LABEL'] = fields['type']
                 del fields['type']
 
+                if 'checklists' in fields:
+                    del fields['checklists']
+
+                fields['org'] = course.id.org
+                fields['course'] = course.id.course
+                fields['run'] = course.id.run
+                fields['course_key'] = unicode(course.id)
+
                 blocks_by_type[block_type].append(fields)
-
-
 
             for item in items:
                 if item.has_children:
@@ -104,9 +104,29 @@ class Command(BaseCommand):
         rows = [[':START_ID', ':END_ID']] if create else []
         rows.extend(relationships)
         mode = 'w' if create else 'a'
-        with open('/tmp/relationships.csv', mode) as csvfile:
-            writer = UnicodeWriter(csvfile)
-            writer.writerows(rows)
+        with open('relationships.tsv', mode) as csvfile:
+            self._write_results_to_tsv(rows, csvfile)
+
+
+    def _write_results_to_tsv(self, rows, output_file):
+        """
+        Writes each row to a TSV file.
+        Fields are separated by tabs, no quote character.
+        Output would be encoded as utf-8.
+        All embeded tabs(\t), newlines(\n), and carriage returns(\r) are escaped.
+        """
+
+        writer = csv.writer(output_file, delimiter="\t", quoting=csv.QUOTE_NONE, quotechar='', lineterminator='\n')
+        converted_rows = []
+        for row in rows:
+            converted_row = [self._normalize_value(val) for val in row]
+            converted_rows.append(converted_row)
+        writer.writerows(converted_rows)
+
+
+    def _normalize_value(self, value):
+        if value is None: value='NULL'
+        return unicode(value).encode('utf-8').replace('\\', '\\\\').replace('\r', '\\r').replace('\t','\\t').replace('\n', '\\n')
 
 
     def add_to_csvs_from_blocks(self, blocks_by_type):
@@ -128,74 +148,7 @@ class Command(BaseCommand):
                 rows.append(row)
 
             mode = 'w' if create else 'a'
-            with open('/tmp/{}.csv'.format(block_type), mode) as csvfile:
-                writer = UnicodeWriter(csvfile)
-                writer.writerows(rows)
+            with open('{}.tsv'.format(block_type), mode) as csvfile:
+                self._write_results_to_tsv(rows, csvfile)
 
 
-
-
-
-
-
-
-import csv, codecs, cStringIO
-
-class UTF8Recoder:
-    """
-    Iterator that reads an encoded stream and reencodes the input to UTF-8
-    """
-    def __init__(self, f, encoding):
-        self.reader = codecs.getreader(encoding)(f)
-
-    def __iter__(self):
-        return self
-
-    def next(self):
-        return self.reader.next().encode("utf-8")
-
-class UnicodeReader:
-    """
-    A CSV reader which will iterate over lines in the CSV file "f",
-    which is encoded in the given encoding.
-    """
-
-    def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
-        f = UTF8Recoder(f, encoding)
-        self.reader = csv.reader(f, dialect=dialect, **kwds)
-
-    def next(self):
-        row = self.reader.next()
-        return [unicode(s, "utf-8") for s in row]
-
-    def __iter__(self):
-        return self
-
-class UnicodeWriter:
-    """
-    A CSV writer which will write rows to CSV file "f",
-    which is encoded in the given encoding.
-    """
-
-    def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
-        # Redirect output to a queue
-        self.queue = cStringIO.StringIO()
-        self.writer = csv.writer(self.queue, dialect=dialect, **kwds)
-        self.stream = f
-        self.encoder = codecs.getincrementalencoder(encoding)()
-
-    def writerow(self, row):
-        self.writer.writerow([s.encode("utf-8") for s in row])
-        # Fetch UTF-8 output from the queue ...
-        data = self.queue.getvalue()
-        data = data.decode("utf-8")
-        # ... and reencode it into the target encoding
-        data = self.encoder.encode(data)
-        # write to the target stream
-        self.stream.write(data)
-        # empty queue
-        self.queue.truncate(0)
-
-    def writerows(self, rows):
-        for row in rows:
-            self.writerow(row)
