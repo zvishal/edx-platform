8f92128ca1e0c402adf24dd888e2e3d411b1cd27
John Eskew <jeskew@edx.org>
2015-12-21 17:08:41 -0500

    Re-add XML_COURSE_DIRS

--- lms/djangoapps/courseware/management/commands/tests/test_dump_course.py
+++ lms/djangoapps/courseware/management/commands/tests/test_dump_course.py
@@ -24,6 +24,7 @@ from xmodule.modulestore.tests.factories import CourseFactory
 from xmodule.modulestore.xml_importer import import_course_from_xml
 
 DATA_DIR = settings.COMMON_TEST_DATA_ROOT
+XML_COURSE_DIRS = ['toy', 'simple']
 MAPPINGS = {}
 
 TEST_DATA_MIXED_XML_MODULESTORE = mixed_store_config(
