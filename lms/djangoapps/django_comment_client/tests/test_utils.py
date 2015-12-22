13da73c86879fce9d852cd0f81ee3d5bb1e4ac84
John Eskew <jeskew@edx.org>
2015-12-22 16:36:53 -0500

    Import test course instead of using XML-backed course.

--- lms/djangoapps/django_comment_client/tests/test_utils.py
+++ lms/djangoapps/django_comment_client/tests/test_utils.py
@@ -23,7 +23,7 @@ from student.tests.factories import UserFactory, AdminFactory, CourseEnrollmentF
 from openedx.core.djangoapps.content.course_structures.models import CourseStructure
 from openedx.core.djangoapps.util.testing import ContentGroupTestCase
 from student.roles import CourseStaffRole
-from xmodule.modulestore.tests.factories import CourseFactory, ItemFactory
+from xmodule.modulestore.tests.factories import CourseFactory, ItemFactory, ToyCourseFactory
 from xmodule.modulestore.tests.django_utils import ModuleStoreTestCase, TEST_DATA_MIXED_TOY_MODULESTORE
 from xmodule.modulestore.django import modulestore
 from opaque_keys.edx.locator import CourseLocator
@@ -1130,7 +1130,7 @@ class IsCommentableCohortedTestCase(ModuleStoreTestCase):
         Make sure that course is reloaded every time--clear out the modulestore.
         """
         super(IsCommentableCohortedTestCase, self).setUp()
-        self.toy_course_key = CourseLocator("edX", "toy", "2012_Fall", deprecated=True)
+        self.toy_course_key = ToyCourseFactory.create().id
 
     def test_is_commentable_cohorted(self):
         course = modulestore().get_course(self.toy_course_key)
