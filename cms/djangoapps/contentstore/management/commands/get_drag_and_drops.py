3f8ac1fbbda396922dbbd5281bc1e48fb4be3420
Adam Palay <adam@edx.org>
2016-01-29 11:18:40 -0500

    add objgraph

--- cms/djangoapps/contentstore/management/commands/get_drag_and_drops.py
+++ cms/djangoapps/contentstore/management/commands/get_drag_and_drops.py
@@ -1,4 +1,5 @@
 import csv
+import gc
 
 from django.core.management.base import BaseCommand
 from xmodule.modulestore.django import modulestore
@@ -7,13 +8,14 @@ from collections import defaultdict
 from django.utils import timezone
 
 from lxml import etree
+import objgraph
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
 
-        ms = modulestore()._get_modulestore_by_type('split')
+        ms = modulestore()
 
         courses = ms.get_courses()
 
@@ -21,8 +23,10 @@ class Command(BaseCommand):
 
         amount_of_courses = len(courses)
 
+        objgraph.show_growth()
+
         for index, course in enumerate(courses):
-            print "doing course \t{} / {}".format(index, amount_of_courses)
+            print "doing course \t{} / {}: {}".format(index, amount_of_courses, course.id)
 
             number_of_dd = 0
             for item in ms.get_items(course.id, include_orphans=False):
@@ -37,16 +41,19 @@ class Command(BaseCommand):
                         if str(element.tag) == "drag_and_drop_input":
                             number_of_dd += 1
 
-            row = [number_of_dd, unicode(course.id), not self.course_is_closed(course)]
-
-
-            rows.append(row)
-
-        print rows
+            if number_of_dd:
+                row = [number_of_dd, unicode(course.id), not self.course_is_closed(course)]
+                rows.append(row)
 
         with open("/tmp/dd.csv", 'w') as csvfile:
             writer = csv.writer(csvfile)
             writer.writerows(rows)
+
+        course = None
+        rows = None
+        gc.collect()
+
+        objgraph.show_growth(100)
 
 
     def course_is_closed(self, course):
